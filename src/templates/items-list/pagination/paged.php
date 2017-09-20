9f3aa9abc58dbdd3273b95d272dda231c5f5ca56
nk-o <nkdevinfo@gmail.com>
2017-09-20 11:24:11 +0300

    paged arrows changed to fontawesome icons

--- src/templates/items-list/pagination/paged.php
+++ src/templates/items-list/pagination/paged.php
@@ -14,7 +14,15 @@
         ?>
         <li class="<?php echo esc_attr( $item['class'] ); ?>">
             <?php if ( $item['url'] ) : ?>
-                <a href="<?php echo esc_url( $item['url'] ); ?>"><?php echo esc_html( $item['label'] ); ?></a>
+                <a href="<?php echo esc_url( $item['url'] ); ?>">
+                    <?php if ( $item['is_prev_arrow'] ) : ?>
+                        <span class="fa fa-angle-left"></span>
+                    <?php elseif ( $item['is_next_arrow'] ) : ?>
+                        <span class="fa fa-angle-right"></span>
+                    <?php else : ?>
+                        <?php echo esc_html( $item['label'] ); ?>
+                    <?php endif; ?>
+                </a>
             <?php else : ?>
                 <span><?php echo esc_html( $item['label'] ); ?></span>
             <?php endif; ?>
