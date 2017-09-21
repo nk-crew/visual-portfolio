66d1941d0c95d2354f9f2720e6b5242f78e52351
nk-o <nkdevinfo@gmail.com>
2017-09-21 23:46:59 +0300

    fixed iframe height calculation

--- src/assets/admin/js/script.js
+++ src/assets/admin/js/script.js
@@ -93,9 +93,10 @@
 
     // resize iframe
     if ( $.fn.iFrameResize ) {
+        var isOldIE = (navigator.userAgent.indexOf('MSIE') !== -1);
         $frame.iFrameResize({
             interval: 10,
-            resizeFrom: 'child'
+            heightCalculationMethod: isOldIE ? 'max' : 'lowestElement'
         });
     }
 
