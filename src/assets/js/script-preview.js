8b6d47df2cf5ab9ca36663402feec415b95ecccf
nk-o <nkdevinfo@gmail.com>
2017-09-26 12:34:30 +0300

    fixed iframe height calculation if in theme set html height 100%;

--- src/assets/js/script-preview.js
+++ src/assets/js/script-preview.js
@@ -4,8 +4,14 @@
  * Author  : nK https://nkdev.info
  */
 (function ($) {
-    $('#vp_preview').on('click', '.vp-portfolio__item, .vp-portfolio__item a', function (e) {
+    var $portfolio = $('#vp_preview > .vp-portfolio');
+    $portfolio.on('click', '.vp-portfolio__item, .vp-portfolio__item a', function (e) {
         e.preventDefault();
         e.stopPropagation();
     });
+    window.iFrameResizer = {
+        heightCalculationMethod: function () {
+            return $portfolio.outerHeight(true);
+        }
+    };
 }(jQuery));
