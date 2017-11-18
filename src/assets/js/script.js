496d4140846b3d6d3d877d33a9290fe9535123d0
nk-o <nkdevinfo@gmail.com>
2017-11-18 23:23:53 +0300

    dont create markup for photoswipe again and again if on the page > 1 visual portfolio with popup gallery

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -789,42 +789,44 @@
         }
 
         // prepare photoswipe markup
-        var markup = '' +
-            '<div class="pswp vp-pswp vp-pswp-uid-' + self.uid + '" tabindex="-1" role="dialog" aria-hidden="true">' +
-            '   <div class="pswp__bg"></div>' +
-            '   <div class="pswp__scroll-wrap">' +
-            '      <div class="pswp__container">' +
-            '          <div class="pswp__item"></div>' +
-            '          <div class="pswp__item"></div>' +
-            '          <div class="pswp__item"></div>' +
-            '      </div>' +
-            '      <div class="pswp__ui pswp__ui--hidden">' +
-            '          <div class="pswp__top-bar">' +
-            '              <div class="pswp__counter"></div>' +
-            '              <a class="pswp__button pswp__button--close" title="' + VPData.__.pswp_close + '"></a>' +
-            '              <a class="pswp__button pswp__button--share" title="' + VPData.__.pswp_share + '"></a>' +
-            '              <a class="pswp__button pswp__button--fs" title="' + VPData.__.pswp_fs + '"></a>' +
-            '              <a class="pswp__button pswp__button--zoom" title="' + VPData.__.pswp_zoom + '"></a>' +
-            '          </div>' +
-            '          <div class="pswp__preloader">' +
-            '              <div class="pswp__preloader__icn">' +
-            '                  <div class="pswp__preloader__cut">' +
-            '                      <div class="pswp__preloader__donut"></div>' +
-            '                  </div>' +
-            '              </div>' +
-            '          </div>' +
-            '          <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">' +
-            '              <div class="pswp__share-tooltip"></div>' +
-            '          </div>' +
-            '          <a class="pswp__button pswp__button--arrow--left" title="' + VPData.__.pswp_prev + '"></a>' +
-            '          <a class="pswp__button pswp__button--arrow--right" title="' + VPData.__.pswp_next + '"></a>' +
-            '          <div class="pswp__caption">' +
-            '              <div class="pswp__caption__center"></div>' +
-            '          </div>' +
-            '      </div>' +
-            '   </div>' +
-            '</div>';
-        $('body').append(markup);
+        if ( !$('.vp-pswp').length ) {
+            var markup = '' +
+                '<div class="pswp vp-pswp vp-pswp-uid-' + self.uid + '" tabindex="-1" role="dialog" aria-hidden="true">' +
+                '   <div class="pswp__bg"></div>' +
+                '   <div class="pswp__scroll-wrap">' +
+                '      <div class="pswp__container">' +
+                '          <div class="pswp__item"></div>' +
+                '          <div class="pswp__item"></div>' +
+                '          <div class="pswp__item"></div>' +
+                '      </div>' +
+                '      <div class="pswp__ui pswp__ui--hidden">' +
+                '          <div class="pswp__top-bar">' +
+                '              <div class="pswp__counter"></div>' +
+                '              <a class="pswp__button pswp__button--close" title="' + VPData.__.pswp_close + '"></a>' +
+                '              <a class="pswp__button pswp__button--share" title="' + VPData.__.pswp_share + '"></a>' +
+                '              <a class="pswp__button pswp__button--fs" title="' + VPData.__.pswp_fs + '"></a>' +
+                '              <a class="pswp__button pswp__button--zoom" title="' + VPData.__.pswp_zoom + '"></a>' +
+                '          </div>' +
+                '          <div class="pswp__preloader">' +
+                '              <div class="pswp__preloader__icn">' +
+                '                  <div class="pswp__preloader__cut">' +
+                '                      <div class="pswp__preloader__donut"></div>' +
+                '                  </div>' +
+                '              </div>' +
+                '          </div>' +
+                '          <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">' +
+                '              <div class="pswp__share-tooltip"></div>' +
+                '          </div>' +
+                '          <a class="pswp__button pswp__button--arrow--left" title="' + VPData.__.pswp_prev + '"></a>' +
+                '          <a class="pswp__button pswp__button--arrow--right" title="' + VPData.__.pswp_next + '"></a>' +
+                '          <div class="pswp__caption">' +
+                '              <div class="pswp__caption__center"></div>' +
+                '          </div>' +
+                '      </div>' +
+                '   </div>' +
+                '</div>';
+            $('body').append(markup);
+        }
 
         // init code
         var parseThumbnailElements = function (el) {
