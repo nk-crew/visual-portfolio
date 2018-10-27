cae8bd6931deb41443e4d8de62b5517dc6cb4221
Nikita <nkdevinfo@gmail.com>
2018-10-27 18:37:11 +0300

    added condition to show meta block in templates

--- src/templates/items-list/items-style/emerge/meta.php
+++ src/templates/items-list/items-style/emerge/meta.php
@@ -11,72 +11,79 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+$show_meta = $opts['show_title'] && $args['title'] ||
+    $opts['show_date'] ||
+    $opts['show_excerpt'] && $args['excerpt'] ||
+    $opts['show_categories'] && $args['categories'] && ! empty( $args['categories'] );
+
 ?>
 
 <div class="vp-portfolio__item-overlay vp-portfolio__item-align-<?php echo esc_attr( $opts['align'] ); ?>">
-    <div class="vp-portfolio__item-meta">
-        <?php
+    <?php if ( $show_meta ) : ?>
+        <div class="vp-portfolio__item-meta">
+            <?php
 
-        // Show Title.
-        if ( $opts['show_title'] && $args['title'] ) {
-            ?>
-            <h2 class="vp-portfolio__item-meta-title">
-                <?php
-                if ( $args['url'] ) {
-                    ?>
-                    <a href="<?php echo esc_url( $args['url'] ); ?>">
-                        <?php echo esc_html( $args['title'] ); ?>
-                    </a>
-                    <?php
-                } else {
-                    echo esc_html( $args['title'] );
-                }
+            // Show Title.
+            if ( $opts['show_title'] && $args['title'] ) {
                 ?>
-            </h2>
-            <?php
-        }
-
-        // Show Date.
-        if ( $opts['show_date'] ) {
-            ?>
-            <div class="vp-portfolio__item-meta-date">
-                <?php echo esc_html( $args['published'] ); ?>
-            </div>
-            <?php
-        }
-
-        // Show Excerpt.
-        if ( $opts['show_excerpt'] && $args['excerpt'] ) {
-            ?>
-            <div class="vp-portfolio__item-meta-excerpt">
-                <?php echo esc_html( $args['excerpt'] ); ?>
-            </div>
-            <?php
-        }
-
-        // Show Categories.
-        if ( $opts['show_categories'] && $args['categories'] && ! empty( $args['categories'] ) ) {
-            ?>
-            <ul class="vp-portfolio__item-meta-categories">
-                <?php
-                $count = $opts['categories_count'];
-                foreach ( $args['categories'] as $category ) {
-                    if ( ! $count ) {
-                        break;
+                <h2 class="vp-portfolio__item-meta-title">
+                    <?php
+                    if ( $args['url'] ) {
+                        ?>
+                        <a href="<?php echo esc_url( $args['url'] ); ?>">
+                            <?php echo esc_html( $args['title'] ); ?>
+                        </a>
+                        <?php
+                    } else {
+                        echo esc_html( $args['title'] );
                     }
                     ?>
-                    <li class="vp-portfolio__item-meta-category">
-                        <a href="<?php echo esc_html( $category['url'] ); ?>">
-                            <?php echo esc_html( $category['label'] ); ?>
-                        </a>
-                    </li>
-                    <?php
-                    $count--;
-                }
+                </h2>
+                <?php
+            }
+
+            // Show Date.
+            if ( $opts['show_date'] ) {
                 ?>
-            </ul>
-            <?php
-        }
-        ?>
-    </div>
+                <div class="vp-portfolio__item-meta-date">
+                    <?php echo esc_html( $args['published'] ); ?>
+                </div>
+                <?php
+            }
+
+            // Show Excerpt.
+            if ( $opts['show_excerpt'] && $args['excerpt'] ) {
+                ?>
+                <div class="vp-portfolio__item-meta-excerpt">
+                    <?php echo esc_html( $args['excerpt'] ); ?>
+                </div>
+                <?php
+            }
+
+            // Show Categories.
+            if ( $opts['show_categories'] && $args['categories'] && ! empty( $args['categories'] ) ) {
+                ?>
+                <ul class="vp-portfolio__item-meta-categories">
+                    <?php
+                    $count = $opts['categories_count'];
+                    foreach ( $args['categories'] as $category ) {
+                        if ( ! $count ) {
+                            break;
+                        }
+                        ?>
+                        <li class="vp-portfolio__item-meta-category">
+                            <a href="<?php echo esc_html( $category['url'] ); ?>">
+                                <?php echo esc_html( $category['label'] ); ?>
+                            </a>
+                        </li>
+                        <?php
+                        $count--;
+                    }
+                    ?>
+                </ul>
+                <?php
+            }
+            ?>
+        </div>
+    <?php endif; ?>
 </div>
--- src/templates/items-list/items-style/fly/meta.php
+++ src/templates/items-list/items-style/fly/meta.php
@@ -15,6 +15,12 @@ $tag = 'a';
 if ( ! $args['url'] ) {
     $tag = 'span';
 }
+
+$show_meta = $opts['show_icon'] && $opts['icon'] ||
+    $opts['show_title'] && $args['title'] ||
+    $opts['show_date'] ||
+    $opts['show_excerpt'] && $args['excerpt'] ||
+    $opts['show_categories'] && $args['categories'] && ! empty( $args['categories'] );
 ?>
 
 <<?php echo esc_html( $tag ); ?>
@@ -22,68 +28,70 @@ if ( ! $args['url'] ) {
         href="<?php echo esc_url( $args['url'] ); ?>"
     <?php endif; ?>
     class="vp-portfolio__item-overlay vp-portfolio__item-align-<?php echo esc_attr( $opts['align'] ); ?>">
-    <div class="vp-portfolio__item-meta">
-        <?php
-
-        // Show Icon.
-        if ( $opts['show_icon'] && $opts['icon'] ) {
-            ?>
-            <div class="vp-portfolio__item-meta-icon">
-                <span class="<?php echo esc_attr( isset( $args['format_video_url'] ) ? $opts['icon_video'] : $opts['icon'] ); ?>"></span>
-            </div>
+    <?php if ( $show_meta ) : ?>
+        <div class="vp-portfolio__item-meta">
             <?php
-        }
 
-        // Show Title.
-        if ( $opts['show_title'] && $args['title'] ) {
-            ?>
-            <h2 class="vp-portfolio__item-meta-title">
-                <?php
-                echo esc_html( $args['title'] );
+            // Show Icon.
+            if ( $opts['show_icon'] && $opts['icon'] ) {
                 ?>
-            </h2>
-            <?php
-        }
-
-        // Show Date.
-        if ( $opts['show_date'] ) {
-            ?>
-            <div class="vp-portfolio__item-meta-date">
-                <?php echo esc_html( $args['published'] ); ?>
-            </div>
-            <?php
-        }
-
-        // Show Excerpt.
-        if ( $opts['show_excerpt'] && $args['excerpt'] ) {
-            ?>
-            <div class="vp-portfolio__item-meta-excerpt">
-                <?php echo esc_html( $args['excerpt'] ); ?>
-            </div>
-            <?php
-        }
-
-        // Show Categories.
-        if ( $opts['show_categories'] && $args['categories'] && ! empty( $args['categories'] ) ) {
-            ?>
-            <ul class="vp-portfolio__item-meta-categories">
+                <div class="vp-portfolio__item-meta-icon">
+                    <span class="<?php echo esc_attr( isset( $args['format_video_url'] ) ? $opts['icon_video'] : $opts['icon'] ); ?>"></span>
+                </div>
                 <?php
-                $count = $opts['categories_count'];
-                foreach ( $args['categories'] as $category ) {
-                    if ( ! $count ) {
-                        break;
+            }
+
+            // Show Title.
+            if ( $opts['show_title'] && $args['title'] ) {
+                ?>
+                <h2 class="vp-portfolio__item-meta-title">
+                    <?php
+                    echo esc_html( $args['title'] );
+                    ?>
+                </h2>
+                <?php
+            }
+
+            // Show Date.
+            if ( $opts['show_date'] ) {
+                ?>
+                <div class="vp-portfolio__item-meta-date">
+                    <?php echo esc_html( $args['published'] ); ?>
+                </div>
+                <?php
+            }
+
+            // Show Excerpt.
+            if ( $opts['show_excerpt'] && $args['excerpt'] ) {
+                ?>
+                <div class="vp-portfolio__item-meta-excerpt">
+                    <?php echo esc_html( $args['excerpt'] ); ?>
+                </div>
+                <?php
+            }
+
+            // Show Categories.
+            if ( $opts['show_categories'] && $args['categories'] && ! empty( $args['categories'] ) ) {
+                ?>
+                <ul class="vp-portfolio__item-meta-categories">
+                    <?php
+                    $count = $opts['categories_count'];
+                    foreach ( $args['categories'] as $category ) {
+                        if ( ! $count ) {
+                            break;
+                        }
+                        ?>
+                        <li class="vp-portfolio__item-meta-category">
+                            <span><?php echo esc_html( $category['label'] ); ?></span>
+                        </li>
+                        <?php
+                        $count--;
                     }
                     ?>
-                    <li class="vp-portfolio__item-meta-category">
-                        <span><?php echo esc_html( $category['label'] ); ?></span>
-                    </li>
-                    <?php
-                    $count--;
-                }
-                ?>
-            </ul>
-            <?php
-        }
-        ?>
-    </div>
+                </ul>
+                <?php
+            }
+            ?>
+        </div>
+    <?php endif; ?>
 </<?php echo esc_html( $tag ); ?>>
