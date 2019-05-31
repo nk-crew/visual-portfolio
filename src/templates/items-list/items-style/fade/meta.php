b6ac6c644cbdfa822e3bbcf94aa07059519c7f57
Nikita <nkdevinfo@gmail.com>
2019-05-31 12:09:32 +0300

    fixed <a> tag in fly and fade effect when all meta disabled

--- src/templates/items-list/items-style/fade/meta.php
+++ src/templates/items-list/items-style/fade/meta.php
@@ -25,19 +25,20 @@ $show_meta = $opts['show_icon'] && $opts['icon'] ||
 ?>
 
 <figcaption class="vp-portfolio__item-overlay vp-portfolio__item-align-<?php echo esc_attr( $opts['align'] ); ?>">
-    <?php if ( $show_meta ) : ?>
-        <<?php echo esc_html( $tag ); ?>
-            <?php if ( $args['url'] ) : ?>
-                href="<?php echo esc_url( $args['url'] ); ?>"
-                <?php
-                if ( isset( $args['url_target'] ) && $args['url_target'] ) :
-                    ?>
-                    target="<?php echo esc_attr( $args['url_target'] ); ?>"
-                    <?php
-                endif;
+    <<?php echo esc_html( $tag ); ?>
+        <?php if ( $args['url'] ) : ?>
+            href="<?php echo esc_url( $args['url'] ); ?>"
+            <?php
+            if ( isset( $args['url_target'] ) && $args['url_target'] ) :
                 ?>
-            <?php endif; ?>
-                class="vp-portfolio__item-meta">
+                target="<?php echo esc_attr( $args['url_target'] ); ?>"
+                <?php
+            endif;
+            ?>
+        <?php endif; ?>
+        class="vp-portfolio__item-meta">
+
+        <?php if ( $show_meta ) : ?>
             <?php
 
             // Show Icon.
@@ -100,6 +101,6 @@ $show_meta = $opts['show_icon'] && $opts['icon'] ||
                 <?php
             }
             ?>
-        </<?php echo esc_html( $tag ); ?>>
-    <?php endif; ?>
+        <?php endif; ?>
+    </<?php echo esc_html( $tag ); ?>>
 </figcaption>
