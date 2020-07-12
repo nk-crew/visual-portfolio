6f2c943ac5841e83365759523d81addd780945fc
Nikita <nkdevinfo@gmail.com>
2020-07-12 18:17:19 +0300

    changed Video metabox icon and added a little description

--- src/gutenberg/custom-post-meta/video.js
+++ src/gutenberg/custom-post-meta/video.js
@@ -135,8 +135,16 @@ class VpVideoComponent extends Component {
             <PluginDocumentSettingPanel
                 name="VPVideo"
                 title={ __( 'Video', '@@text_domain' ) }
-                icon="format-video"
+                icon={ (
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.25 10C19.25 15.1086 15.1086 19.25 10 19.25C4.89137 19.25 0.75 15.1086 0.75 10C0.75 4.89137 4.89137 0.75 10 0.75C15.1086 0.75 19.25 4.89137 19.25 10Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="transparent" />
+                        <path d="M8 12.6326V7.36671C8.00011 7.30082 8.01856 7.23618 8.05342 7.17955C8.08828 7.12293 8.13826 7.0764 8.19812 7.04485C8.25798 7.0133 8.32552 6.99789 8.39367 7.00023C8.46181 7.00257 8.52805 7.02258 8.58544 7.05816L12.8249 9.69035C12.8786 9.72358 12.9228 9.76933 12.9534 9.82337C12.984 9.87742 13 9.93803 13 9.99963C13 10.0612 12.984 10.1218 12.9534 10.1759C12.9228 10.2299 12.8786 10.2757 12.8249 10.3089L8.58544 12.9418C8.52805 12.9774 8.46181 12.9974 8.39367 12.9998C8.32552 13.0021 8.25798 12.9867 8.19812 12.9551C8.13826 12.9236 8.08828 12.8771 8.05342 12.8204C8.01856 12.7638 8.00011 12.6992 8 12.6333V12.6326Z" fill="currentColor" />
+                    </svg>
+                ) }
             >
+                <PanelRow>
+                    <p className="description">{ __( 'Video will be used in Visual Portfolio layouts only.', '@@text_domain' ) }</p>
+                </PanelRow>
                 <PanelRow>
                     <TextControl
                         label={ __( 'Video URL', '@@text_domain' ) }
