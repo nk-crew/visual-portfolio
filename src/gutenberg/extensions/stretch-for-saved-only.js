9b9174cd4da08887cae6a58c2225e02651edb5dc
Nikita <nkdevinfo@gmail.com>
2020-07-20 22:41:52 +0300

    fixed displaying Stretch option in Saved Layouts

--- src/gutenberg/extensions/stretch-for-saved-only.js
+++ src/gutenberg/extensions/stretch-for-saved-only.js
@@ -5,13 +5,9 @@ const {
     addFilter,
 } = wp.hooks;
 
-const {
-    VPSavedLayoutVariables,
-} = window;
-
 // Allow Stretch control on Saved Layouts editor only.
 addFilter( 'vpf.editor.controls-render-data', 'vpf/editor/controls-render-data/customize-controls', ( data ) => {
-    if ( 'stretch' === data.name && ! VPSavedLayoutVariables ) {
+    if ( 'stretch' === data.name && ! window.VPSavedLayoutVariables ) {
         data = {
             ...data,
             skip: true,
