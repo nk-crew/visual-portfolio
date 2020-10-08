5c9bb5360792cb71a7618bbba144ecd1a8679db1
Nikita <nkdevinfo@gmail.com>
2020-10-08 15:13:51 +0300

    changed grid isotope layout mode to our own custom script

--- src/assets/js/layout-grid.js
+++ src/assets/js/layout-grid.js
@@ -9,107 +9,113 @@ const {
 
 /* eslint-disable no-underscore-dangle */
 
-// fix grid items position for Grid layout and FireFox.
-// https://wordpress.org/support/topic/gallery-difference-between-firefox-and-all-other-browsers/
+//
+// Our custom Grid layout for Isotope.
+//
+// * fixes grid items position in FireFox - https://wordpress.org/support/topic/gallery-difference-between-firefox-and-all-other-browsers/
+//
 if ( 'undefined' !== typeof window.Isotope && 'undefined' !== typeof window.Isotope.LayoutMode ) {
-    const GridMode = window.Isotope.LayoutMode.modes.fitRows;
+    const VPRows = window.Isotope.LayoutMode.create( 'vpRows' );
+    const proto = VPRows.prototype;
 
-    if ( GridMode ) {
-        GridMode.prototype._resetLayout = function() {
+    proto.measureColumns = function() {
+        // set items, used if measuring first item
+        this.items = this.isotope.filteredItems;
+
+        this.getContainerWidth();
+
+        // if columnWidth is 0, default to outerWidth of first item
+        if ( ! this.columnWidth ) {
+            const firstItem = this.items[ 0 ];
+            const firstItemElem = firstItem && firstItem.element;
+
+            // columnWidth fall back to item of first element
+            this.columnWidth = ( firstItemElem && window.getSize( firstItemElem ).outerWidth )
+                // if first elem has no width, default to size of container
+                || this.containerWidth;
+        }
+
+        this.columnWidth += this.gutter;
+
+        // calculate columns
+        const containerWidth = this.containerWidth + this.gutter;
+        let cols = containerWidth / this.columnWidth;
+
+        // fix rounding errors, typically with gutters
+        const excess = this.columnWidth - ( containerWidth % this.columnWidth );
+
+        // if overshoot is less than a pixel, round up, otherwise floor it
+        const mathMethod = excess && 1 > excess ? 'round' : 'floor';
+
+        cols = Math[ mathMethod ]( cols );
+        this.cols = Math.max( cols, 1 );
+    };
+
+    proto.getContainerWidth = function() {
+        // container is parent if fit width
+        const isFitWidth = this._getOption ? this._getOption( 'fitWidth' ) : false;
+        const container = isFitWidth ? this.element.parentNode : this.element;
+
+        // check that this.size and size are there
+        // IE8 triggers resize on body size change, so they might not be
+        const size = window.getSize( container );
+        this.containerWidth = size && size.innerWidth;
+    };
+
+    proto._resetLayout = function() {
+        this.x = 0;
+        this.y = 0;
+        this.maxY = 0;
+        this.horizontalColIndex = 0;
+
+        this._getMeasurement( 'columnWidth', 'outerWidth' );
+        this._getMeasurement( 'gutter', 'outerWidth' );
+        this.measureColumns();
+    };
+
+    proto._getItemLayoutPosition = function( item ) {
+        item.getSize();
+
+        // how many columns does this brick span
+        const remainder = item.size.outerWidth % this.columnWidth;
+        const mathMethod = remainder && 1 > remainder ? 'round' : 'ceil';
+
+        // round if off by 1 pixel, otherwise use ceil
+        let colSpan = Math[ mathMethod ]( item.size.outerWidth / this.columnWidth );
+        colSpan = Math.min( colSpan, this.cols );
+
+        let col = this.horizontalColIndex % this.cols;
+        const isOver = 1 < colSpan && col + colSpan > this.cols;
+
+        // shift to next row if item can't fit on current row
+        col = isOver ? 0 : col;
+
+        // don't let zero-size items take up space
+        const hasSize = item.size.outerWidth && item.size.outerHeight;
+        this.horizontalColIndex = hasSize ? col + colSpan : this.horizontalColIndex;
+
+        const itemWidth = item.size.outerWidth + this.gutter;
+
+        // if this element cannot fit in the current row
+        if ( 0 !== this.x && 1 === this.horizontalColIndex ) {
             this.x = 0;
-            this.y = 0;
-            this.maxY = 0;
-            this.horizontalColIndex = 0;
+            this.y = this.maxY;
+        }
 
-            this._getMeasurement( 'columnWidth', 'outerWidth' );
-            this._getMeasurement( 'gutter', 'outerWidth' );
-            this.measureColumns();
+        const position = {
+            x: this.x,
+            y: this.y,
         };
 
-        GridMode.prototype.measureColumns = function() {
-            // set items, used if measuring first item
-            this.items = this.isotope.filteredItems;
+        this.maxY = Math.max( this.maxY, this.y + item.size.outerHeight );
+        this.x += itemWidth;
 
-            this.getContainerWidth();
+        return position;
+    };
 
-            // if columnWidth is 0, default to outerWidth of first item
-            if ( ! this.columnWidth ) {
-                const firstItem = this.items[ 0 ];
-                const firstItemElem = firstItem && firstItem.element;
-
-                // columnWidth fall back to item of first element
-                this.columnWidth = ( firstItemElem && window.getSize( firstItemElem ).outerWidth )
-                    // if first elem has no width, default to size of container
-                    || this.containerWidth;
-            }
-
-            this.columnWidth += this.gutter;
-
-            // calculate columns
-            const containerWidth = this.containerWidth + this.gutter;
-            let cols = containerWidth / this.columnWidth;
-
-            // fix rounding errors, typically with gutters
-            const excess = this.columnWidth - ( containerWidth % this.columnWidth );
-
-            // if overshoot is less than a pixel, round up, otherwise floor it
-            const mathMethod = excess && 1 > excess ? 'round' : 'floor';
-
-            cols = Math[ mathMethod ]( cols );
-            this.cols = Math.max( cols, 1 );
-        };
-
-        GridMode.prototype.getContainerWidth = function() {
-            // container is parent if fit width
-            const isFitWidth = this._getOption ? this._getOption( 'fitWidth' ) : false;
-            const container = isFitWidth ? this.element.parentNode : this.element;
-
-            // check that this.size and size are there
-            // IE8 triggers resize on body size change, so they might not be
-            const size = window.getSize( container );
-            this.containerWidth = size && size.innerWidth;
-        };
-
-        GridMode.prototype._getItemLayoutPosition = function( item ) {
-            item.getSize();
-
-            // how many columns does this brick span
-            const remainder = item.size.outerWidth % this.columnWidth;
-            const mathMethod = remainder && 1 > remainder ? 'round' : 'ceil';
-
-            // round if off by 1 pixel, otherwise use ceil
-            let colSpan = Math[ mathMethod ]( item.size.outerWidth / this.columnWidth );
-            colSpan = Math.min( colSpan, this.cols );
-
-            let col = this.horizontalColIndex % this.cols;
-            const isOver = 1 < colSpan && col + colSpan > this.cols;
-
-            // shift to next row if item can't fit on current row
-            col = isOver ? 0 : col;
-
-            // don't let zero-size items take up space
-            const hasSize = item.size.outerWidth && item.size.outerHeight;
-            this.horizontalColIndex = hasSize ? col + colSpan : this.horizontalColIndex;
-
-            const itemWidth = item.size.outerWidth + this.gutter;
-
-            // if this element cannot fit in the current row
-            if ( 0 !== this.x && 1 === this.horizontalColIndex ) {
-                this.x = 0;
-                this.y = this.maxY;
-            }
-
-            const position = {
-                x: this.x,
-                y: this.y,
-            };
-
-            this.maxY = Math.max( this.maxY, this.y + item.size.outerHeight );
-            this.x += itemWidth;
-
-            return position;
-        };
-    }
+    proto._getContainerSize = function() {
+        return { height: this.maxY };
+    };
 }
 
 /**
@@ -199,5 +205,5 @@ $( document ).on( 'beforeInitIsotope.vpf', ( event, self, initOptions ) => {
         return;
     }
 
-    initOptions.layoutMode = 'fitRows';
+    initOptions.layoutMode = 'vpRows';
 } );
