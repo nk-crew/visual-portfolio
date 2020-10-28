b41039607f09878783cd2b051ee2a8d9d55e5483
Nikita <nkdevinfo@gmail.com>
2020-10-28 21:46:23 +0300

    better preloader animation

--- src/templates/items-list/wrapper-start.php
+++ src/templates/items-list/wrapper-start.php
@@ -29,5 +29,22 @@ if ( ! defined( 'ABSPATH' ) ) {
     ?>
 >
     <div class="vp-portfolio__preloader-wrap">
-        <div class="vp-portfolio__preloader"><span></span><span></span><span></span><span></span><i></i></div>
+        <div class="vp-portfolio__preloader">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="20" height="20" rx="10" fill="black"/>
+                <mask id="vpf_logo_mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="9" y="6" width="6" height="8">
+                    <path d="M11.5062 13.9L14.0488 6.59998H11.6895L9.40001 13.9H11.5062Z" fill="url(#vpf_logo_paint0_linear)"/>
+                </mask>
+                <g mask="url(#vpf_logo_mask0)">
+                    <path d="M11.5062 13.9L14.0488 6.59998H11.6895L9.40001 13.9H11.5062Z" fill="white"/>
+                </g>
+                <path d="M8.54256 13.9L6 6.59998H8.35933L10.6488 13.9H8.54256Z" fill="white"/>
+                <defs>
+                    <linearGradient id="vpf_logo_paint0_linear" x1="12.9" y1="6.59998" x2="6.6" y2="15.4" gradientUnits="userSpaceOnUse">
+                    <stop/>
+                    <stop offset="1" stop-opacity="0"/>
+                    </linearGradient>
+                </defs>
+            </svg>
+        </div>
     </div>
