6ddbf8ea588e17333536a2af38af566f130917d3
Nikita <nkdevinfo@gmail.com>
2021-05-05 20:31:09 +0300

    fixed lightbox and lazyloading conflicts in Enfold theme

--- src/class-visual-portfolio.php
+++ src/class-visual-portfolio.php
@@ -173,6 +173,7 @@ class Visual_Portfolio {
         require_once $this->plugin_path . 'classes/3rd/plugins/class-vc.php';
         require_once $this->plugin_path . 'classes/3rd/plugins/class-wpml.php';
         require_once $this->plugin_path . 'classes/3rd/themes/class-avada.php';
+        require_once $this->plugin_path . 'classes/3rd/themes/class-enfold.php';
 
         // Migration run after all features.
         require_once $this->plugin_path . 'classes/class-migration.php';
