ac06c84541d7318e9cb8d2ea9d7cb5a06692649c
Nikita <nkdevinfo@gmail.com>
2022-04-29 12:06:52 +0300

    slightly improved styles for Sortable control

--- src/gutenberg/components/sortable-control/index.js
+++ src/gutenberg/components/sortable-control/index.js
@@ -49,7 +49,15 @@ const SortableItem = SortableElement(({ element, sourceOptions, props, state, co
             });
           }}
         >
-          -
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M20 12H4" />
+          </svg>
         </Button>
       ) : null}
     </li>
@@ -141,7 +149,15 @@ export default class SortableControl extends Component {
                       });
                     }}
                   >
-                    +
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+                    </svg>
                   </Button>
                 ) : null}
                 {options[el]}
