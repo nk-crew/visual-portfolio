57a5c9e9548d86de96eb5db6277a76b264f86423
Nikita <nkdevinfo@gmail.com>
2022-05-14 15:39:11 +0300

    removed console.log from frontend script

--- src/classes/3rd/plugins/class-elementor.php
+++ src/classes/3rd/plugins/class-elementor.php
@@ -67,8 +67,6 @@ class Visual_Portfolio_3rd_Elementor {
                         return;
                     }
 
-                    console.log($items);
-
                     addDataAttribute($items);
                 });
             })(window.jQuery);
