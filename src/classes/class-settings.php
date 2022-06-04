0ba336c93102e4dc8dc5bface8f155f4b3d97b3f
Nikita <nkdevinfo@gmail.com>
2022-06-04 19:20:22 +0300

    changed Popup settings texts

--- src/classes/class-settings.php
+++ src/classes/class-settings.php
@@ -161,7 +161,7 @@ class Visual_Portfolio_Settings {
             ),
             array(
                 'id'    => 'vp_popup_gallery',
-                'title' => esc_html__( 'Popup Gallery', '@@text_domain' ),
+                'title' => esc_html__( 'Popup & Lightbox', '@@text_domain' ),
                 'icon'  => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>',
             ),
             array(
@@ -354,8 +354,8 @@ class Visual_Portfolio_Settings {
                 ),
                 array(
                     'name'    => 'images_popup_title',
-                    'label'   => esc_html__( 'Popup Gallery Image Sizes', '@@text_domain' ),
-                    'desc'    => __( 'Image sizes used in popup gallery images.', '@@text_domain' ),
+                    'label'   => esc_html__( 'Lightbox Image Sizes', '@@text_domain' ),
+                    'desc'    => __( 'Image sizes used in lightbox images.', '@@text_domain' ),
                     'type'    => 'section_title',
                 ),
                 array(
@@ -402,8 +402,8 @@ class Visual_Portfolio_Settings {
                 // Default WordPress Images.
                 array(
                     'name'    => 'enable_on_wordpress_images',
-                    'label'   => esc_html__( 'WordPress Images', '@@text_domain' ),
-                    'desc'    => esc_html__( 'Enable popup for WordPress images and galleries.', '@@text_domain' ),
+                    'label'   => esc_html__( 'Lightbox for WordPress Images', '@@text_domain' ),
+                    'desc'    => esc_html__( 'Enable lightbox for WordPress native galleries and images.', '@@text_domain' ),
                     'type'    => 'toggle',
                     'default' => 'off',
                 ),
