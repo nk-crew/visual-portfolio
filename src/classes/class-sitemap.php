7f9363c7646cdeb34ffc3d4aac0176fe9f3fd7d3
Alexey <alexey.fellan@gmail.com>
2022-06-22 16:04:29 +0300

    added parsed block function for sitemap

--- src/classes/class-sitemap.php
+++ src/classes/class-sitemap.php
@@ -2,7 +2,7 @@
 /**
  * Supported Images in Sitemap (SEO).
  *
- * @package @@plugin_name/get
+ * @package @@plugin_name/sitemap
  */
 
 /**
@@ -30,79 +30,109 @@ class Visual_Portfolio_Sitemap {
                 $post_permalink = $entry['loc'];
                 $post_id        = url_to_postid( $post_permalink );
                 $images         = isset( $entry['images'] ) ? $entry['images'] : array();
-                if ( $post_id > 0 ) {
-                    $post         = get_post( $post_id );
-                    $content_post = $post->post_content;
-                    $parse_blocks = parse_blocks( $content_post );
 
-                    if ( ! empty( $parse_blocks ) && is_array( $parse_blocks ) ) {
-                        $block_images = array();
-                        foreach ( $parse_blocks as $block ) {
-                            if (
-                                'visual-portfolio/block' === $block['blockName'] ||
-                                'visual-portfolio/saved' === $block['blockName'] ||
-                                'nk/visual-portfolio' === $block['blockName']
-                            ) {
-                                $options = Visual_Portfolio_Get::get_options( $block['attrs'] );
-                                switch ( $options['content_source'] ) {
-                                    case 'post-based':
-                                        if ( isset( $options['posts_source'] ) ) {
-                                            $query_opts = Visual_Portfolio_Get::get_query_params( $options, false, $options['id'] );
+                if ( 0 === $post_id ) {
+                    $archive_page = Visual_Portfolio_Settings::get_option( 'portfolio_archive_page', 'vp_general' );
 
-                                            $portfolio_query = new WP_Query( $query_opts );
+                    if ( get_permalink( $archive_page ) === $post_permalink ) {
+                        $post_id = $archive_page;
+                    }
+                }
 
-                                            if ( isset( $portfolio_query ) ) {
-                                                while ( $portfolio_query->have_posts() ) {
-                                                    $portfolio_query->the_post();
+                $block_images = $this->parse_images_from_blocks( $post_id );
 
-                                                    $image_id = 'attachment' === get_post_type() ? get_the_ID() : get_post_thumbnail_id( get_the_ID() );
+                if ( ! empty( $block_images ) ) {
+                    foreach ( $block_images as $image ) {
+                        $images[] = (object) array(
+                            'image:loc'     => $image['src'],
+                            'image:caption' => $image['alt'],
+                            'image:title'   => $image['title'],
+                        );
+                    }
+                }
 
-                                                    $image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+                $entry['images'] = $images;
+            }
+        }
 
-                                                    $block_images[] = (object) array(
-                                                        'image:loc'     => wp_get_attachment_image_url( $image_id, 'full' ),
-                                                        'image:caption' => $image_alt,
-                                                        'image:title'   => get_the_title( $image_id ),
-                                                    );
-                                                }
-                                                $portfolio_query->reset_postdata();
+        return $entries;
+    }
 
-                                                // Sometimes, when we use WPBakery Page Builder, without this reset output is wrong.
-                                                wp_reset_postdata();
-                                            }
+    /**
+     * Parse block images.
+     *
+     * @param  int $post_id - Post ID.
+     * @return array
+     */
+    private function parse_images_from_blocks( $post_id ) {
+        $block_images = array();
+        if ( $post_id > 0 ) {
+            $post         = get_post( $post_id );
+            $content_post = $post->post_content;
+            $parse_blocks = parse_blocks( $content_post );
+
+            if ( ! empty( $parse_blocks ) && is_array( $parse_blocks ) ) {
+                foreach ( $parse_blocks as $block ) {
+                    if (
+                        'visual-portfolio/block' === $block['blockName'] ||
+                        'visual-portfolio/saved' === $block['blockName'] ||
+                        'nk/visual-portfolio' === $block['blockName']
+                    ) {
+                        $options = Visual_Portfolio_Get::get_options( $block['attrs'] );
+                        switch ( $options['content_source'] ) {
+                            case 'post-based':
+                                if ( isset( $options['posts_source'] ) ) {
+                                    $query_opts = Visual_Portfolio_Get::get_query_params( $options, false, $options['id'] );
+
+                                    $portfolio_query = new WP_Query( $query_opts );
+
+                                    if ( isset( $portfolio_query ) ) {
+                                        while ( $portfolio_query->have_posts() ) {
+                                            $portfolio_query->the_post();
+
+                                            $image_id = 'attachment' === get_post_type() ? get_the_ID() : get_post_thumbnail_id( get_the_ID() );
+
+                                            $image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+
+                                            $block_images[] = array(
+                                                'src'   => wp_get_attachment_image_url( $image_id, 'full' ),
+                                                'alt'   => $image_alt,
+                                                'title' => get_the_title( $image_id ),
+                                            );
                                         }
-                                        break;
-                                    case 'images':
-                                        if ( isset( $options['images'] ) ) {
-                                            foreach ( $options['images'] as $image ) {
-                                                $image_id = $image['id'];
+                                        $portfolio_query->reset_postdata();
 
-                                                $image_alt = $image['description'] ?? get_post_meta( $image_id, '_wp_attachment_image_alt', true ) ?? '';
-
-                                                $image_title = $image['title'] ?? get_the_title( $image_id );
-
-                                                $image_url = $image['imgUrl'] ?? wp_get_attachment_image_url( $image_id, 'full' );
-
-                                                $block_images[] = (object) array(
-                                                    'image:loc'     => $image_url,
-                                                    'image:caption' => $image_alt,
-                                                    'image:title'   => $image_title,
-                                                );
-                                            }
-                                        }
-                                        break;
+                                        // Sometimes, when we use WPBakery Page Builder, without this reset output is wrong.
+                                        wp_reset_postdata();
+                                    }
                                 }
-                            }
-                        }
-                        if ( ! empty( $block_images ) ) {
-                            $entry['images'] = $block_images;
+                                break;
+                            case 'images':
+                                if ( isset( $options['images'] ) ) {
+                                    foreach ( $options['images'] as $image ) {
+                                        $image_id = $image['id'];
+
+                                        $image_alt = $image['description'] ?? get_post_meta( $image_id, '_wp_attachment_image_alt', true ) ?? '';
+
+                                        $image_title = $image['title'] ?? get_the_title( $image_id );
+
+                                        $image_url = $image['imgUrl'] ?? wp_get_attachment_image_url( $image_id, 'full' );
+
+                                        $block_images[] = array(
+                                            'src'   => $image_url,
+                                            'alt'   => $image_alt,
+                                            'title' => $image_title,
+                                        );
+                                    }
+                                }
+                                break;
                         }
                     }
                 }
             }
         }
 
-        return $entries;
+        return $block_images;
     }
 }
 new Visual_Portfolio_Sitemap();
