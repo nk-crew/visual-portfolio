75025b2370f5b68e5ac71013bee9ad6fa1a669b0
Nikita <nkdevinfo@gmail.com>
2022-08-02 16:35:03 +0300

    fixed rocket loader in iframe preview

--- src/classes/class-preview.php
+++ src/classes/class-preview.php
@@ -230,6 +230,9 @@ class Visual_Portfolio_Preview {
         // Disable the WP admin bar.
         add_filter( 'show_admin_bar', '__return_false' );
 
+        // Avoid Cloudflare's Rocket Loader lazy load the editor iframe.
+        add_filter( 'script_loader_tag', array( $this, 'rocket_loader_filter' ) );
+
         // Enqueue assets.
         wp_enqueue_script( 'iframe-resizer-content', visual_portfolio()->plugin_url . 'assets/vendor/iframe-resizer/js/iframeResizer.contentWindow.min.js', array(), '4.2.11', true );
         wp_enqueue_script( 'visual-portfolio-preview', visual_portfolio()->plugin_url . 'assets/js/preview.min.js', array( 'jquery', 'iframe-resizer-content' ), '@@plugin_version', true );
@@ -265,7 +268,7 @@ class Visual_Portfolio_Preview {
             }
         }
 
-        // Elementor preview.
+        // Elementor widget preview.
         if ( isset( $_REQUEST['vp_preview_type'] ) && 'elementor' === $_REQUEST['vp_preview_type'] && isset( $_REQUEST['vp_preview_frame_id'] ) ) {
             $options[ 'id' ] = esc_attr( wp_unslash( $_REQUEST['vp_preview_frame_id'] ) );
         }
@@ -285,6 +288,17 @@ class Visual_Portfolio_Preview {
                 'class_name' => $class_name,
             )
         );
+    }
+
+    /**
+     * Disable rocket loader in preview.
+     *
+     * @param String $tag - tag string.
+     *
+     * @return String
+     */
+    public function rocket_loader_filter( $tag ) {
+        return str_replace( '<script', '<script data-cfasync="false"', $tag );
     }
 }
 
