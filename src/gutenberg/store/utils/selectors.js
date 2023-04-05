e7b794acb748252bcbbada79577101a7602c8956
Nikita <nkdevinfo@gmail.com>
2023-04-05 19:39:52 +0300

    added more functions to utils store

--- src/gutenberg/store/utils/selectors.js
+++ src/gutenberg/store/utils/selectors.js
@@ -3,10 +3,20 @@
  */
 import conditionCheck from '../../utils/control-condition-check';
 import controlGetValue from '../../utils/control-get-value';
+import getDynamicCSS, {
+  prepareStylesFromParams,
+  hasDynamicCSS,
+} from '../../utils/controls-dynamic-css';
+import { maybeEncode, maybeDecode } from '../../utils/encode-decode';
 
 export function get() {
   return {
     conditionCheck,
     controlGetValue,
+    prepareStylesFromParams,
+    hasDynamicCSS,
+    getDynamicCSS,
+    maybeEncode,
+    maybeDecode,
   };
 }
