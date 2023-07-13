b0c8ff9f75ab4a452787b6c14fcb5fab6dbea78f
Nikita <nkdevinfo@gmail.com>
2023-07-14 01:45:21 +0300

    fixed lazy loading conflict with WP Rocket Delay JS setting

--- src/class-visual-portfolio.php
+++ src/class-visual-portfolio.php
@@ -216,6 +216,7 @@ class Visual_Portfolio {
         require_once $this->plugin_path . 'classes/3rd/plugins/class-sg-cachepress.php';
         require_once $this->plugin_path . 'classes/3rd/plugins/class-tinymce.php';
         require_once $this->plugin_path . 'classes/3rd/plugins/class-vc.php';
+        require_once $this->plugin_path . 'classes/3rd/plugins/class-wp-rocket.php';
         require_once $this->plugin_path . 'classes/3rd/plugins/class-wpml.php';
         require_once $this->plugin_path . 'classes/3rd/themes/class-avada.php';
         require_once $this->plugin_path . 'classes/3rd/themes/class-enfold.php';
