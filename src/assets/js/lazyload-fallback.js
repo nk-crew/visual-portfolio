1aa48137ff521c5499c1a5c4d0a471d1cc86e2c0
Nikita <nkdevinfo@gmail.com>
2023-09-26 15:53:31 +0300

    fixed formatting

--- src/assets/js/lazyload-fallback.js
+++ src/assets/js/lazyload-fallback.js
@@ -5,18 +5,22 @@
 
 // Lazyloaded - remove preloader images placeholder effect.
 document.addEventListener('lazybeforeunveil', (e) => {
-  const vpfImgWrapper = e.target.closest('.vp-portfolio__item-img, .vp-portfolio__thumbnail-img');
+	const vpfImgWrapper = e.target.closest(
+		'.vp-portfolio__item-img, .vp-portfolio__thumbnail-img'
+	);
 
-  if (vpfImgWrapper) {
-    vpfImgWrapper.classList.add('vp-has-lazyloading');
-  }
+	if (vpfImgWrapper) {
+		vpfImgWrapper.classList.add('vp-has-lazyloading');
+	}
 });
 
 document.addEventListener('lazyloaded', (e) => {
-  const vpfImgWrapper = e.target.closest('.vp-portfolio__item-img, .vp-portfolio__thumbnail-img');
+	const vpfImgWrapper = e.target.closest(
+		'.vp-portfolio__item-img, .vp-portfolio__thumbnail-img'
+	);
 
-  if (vpfImgWrapper) {
-    vpfImgWrapper.classList.add('vp-has-lazyloaded');
-    vpfImgWrapper.classList.add('vp-has-lazyloading');
-  }
+	if (vpfImgWrapper) {
+		vpfImgWrapper.classList.add('vp-has-lazyloaded');
+		vpfImgWrapper.classList.add('vp-has-lazyloading');
+	}
 });
