44ab545b18770b74891a2cab614cd03d021ebbd0
Fellan-91 <alexey.fellan@gmail.com>
2023-11-22 15:58:10 +0300

    added support Yoast graph schema for webpage, breadcrumb and potential action target

--- classes/3rd/plugins/class-yoast.php
+++ classes/3rd/plugins/class-yoast.php
@@ -20,12 +20,14 @@ class Visual_Portfolio_3rd_Yoast {
 		// Fixed canonical links.
 		add_filter( 'wpseo_canonical', array( $this, 'canonical' ), 12, 1 );
 		add_filter( 'wpseo_opengraph_url', array( $this, 'canonical' ), 12, 1 );
-		add_filter( 'wpseo_schema_webpage', array( $this, 'graph_schema' ), 12, 4 );
+		add_filter( 'wpseo_schema_webpage', array( $this, 'graph_schema_webpage' ), 12, 1 );
+		add_filter( 'wpseo_schema_breadcrumb', array( $this, 'graph_schema_breadcrumb' ), 12, 1 );
 		add_filter( 'wpseo_opengraph_title', array( $this, 'graph_title' ), 12, 1 );
 	}
 
 	/**
 	 * Optimize url by supported GET variables: vp_page, vp_filter, vp_sort and vp_search.
+	 * Also check term link and replaced it.
 	 *
 	 * @param string $canonical - Not optimized URL.
 	 * @return string
@@ -43,16 +45,38 @@ class Visual_Portfolio_3rd_Yoast {
 	}
 
 	/**
-	 * Allows changing graph piece output.
+	 * Allows changing webpage graph piece output.
 	 *
-	 * @param array $graph_piece - The graph piece to filter.
+	 * @param array $webpage_graph_piece - The webpage graph piece to filter.
 	 * @return array
 	 */
-	public function graph_schema( $graph_piece ) {
-		$graph_piece['@id']  = $this->canonical( $graph_piece['@id'] );
-		$graph_piece['url']  = $this->canonical( $graph_piece['url'] );
-		$graph_piece['name'] = Visual_Portfolio_Archive_Mapping::get_current_term_title() ?? $graph_piece['name'];
-		return $graph_piece;
+	public function graph_schema_webpage( $webpage_graph_piece ) {
+		$webpage_graph_piece['@id']               = $this->canonical( $webpage_graph_piece['@id'] );
+		$webpage_graph_piece['url']               = $this->canonical( $webpage_graph_piece['url'] );
+		$webpage_graph_piece['breadcrumb']['@id'] = $this->canonical_breadcrumb( $webpage_graph_piece['breadcrumb']['@id'] );
+		$webpage_graph_piece['name']              = Visual_Portfolio_Archive_Mapping::get_current_term_title() ?? $webpage_graph_piece['name'];
+
+		if ( isset( $webpage_graph_piece['potentialAction'] ) && ! empty( $webpage_graph_piece['potentialAction'] ) ) {
+			foreach ( $webpage_graph_piece['potentialAction'] as $key => $potential_action ) {
+				if ( isset( $potential_action['target'] ) && isset( $potential_action['@type'] ) && 'ReadAction' === $potential_action['@type'] ) {
+					$webpage_graph_piece['potentialAction'][ $key ]['target'] = $this->canonical( $potential_action['target'] );
+				}
+			}
+		}
+
+		return $webpage_graph_piece;
+	}
+
+	/**
+	 * Allows changing breadcrumb graph piece output.
+	 *
+	 * @param array $breadcrumb_graph_piece - The breadcrumb graph piece to filter.
+	 * @return array
+	 */
+	public function graph_schema_breadcrumb( $breadcrumb_graph_piece ) {
+		$breadcrumb_graph_piece['@id'] = $this->canonical_breadcrumb( $breadcrumb_graph_piece['@id'] );
+
+		return $breadcrumb_graph_piece;
 	}
 
 	/**
@@ -63,6 +87,19 @@ class Visual_Portfolio_3rd_Yoast {
 	 */
 	public function graph_title( $title ) {
 		return Visual_Portfolio_Archive_Mapping::get_current_term_title() ?? $title;
+	}
+
+	/**
+	 * Optimize breadcrumb url by supported GET variables: vp_page, vp_filter, vp_sort and vp_search.
+	 * Also check breadcrumb term link and replaced it.
+	 *
+	 * @param string $breadcrumb - Not optimized breadcrumb URL.
+	 * @return string
+	 */
+	private function canonical_breadcrumb( $breadcrumb ) {
+		return str_contains( $breadcrumb, '#breadcrumb' ) ?
+		str_replace( '#breadcrumb', '', $this->canonical( $breadcrumb ) ) . '#breadcrumb' :
+		$this->canonical( $breadcrumb );
 	}
 }
 new Visual_Portfolio_3rd_Yoast();
