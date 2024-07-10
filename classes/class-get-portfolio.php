7237f25b3f122d27a4d48d3bccc4c60d5fb9335f
Nikita <nkdevinfo@gmail.com>
2024-07-10 17:27:52 +0300

    Update class-get-portfolio.php

--- classes/class-get-portfolio.php
+++ classes/class-get-portfolio.php
@@ -1427,15 +1427,15 @@ class Visual_Portfolio_Get {
 					case 'image_caption':
 					case 'image_alt':
 					case 'image_description':
+						if ( 'date' === $custom_order ) {
+							$custom_order = 'published_time';
+						}
+
 						/**
 						 * We've reworked this code to work correctly with empty sortable values.
 						 * Now images with filled values ​​will be sorted first.
 						 * And empty images will be inserted later at the very end of the array.
 						 */
-						if ( 'date' === $custom_order ) {
-							$custom_order = 'published_time';
-						}
-
 						$images = self::sort_array_by_field( $images, $custom_order, $custom_order_direction );
 						break;
 					case 'rand':
