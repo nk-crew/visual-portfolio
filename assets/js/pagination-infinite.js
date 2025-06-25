c83b3dcd35bf9aa31325fa108a6564078b090dce
Nikita <nkdevinfo@gmail.com>
2025-06-25 13:59:46 +0300

    reset legacy infinite script to initial state

--- assets/js/pagination-infinite.js
+++ assets/js/pagination-infinite.js
@@ -6,30 +6,7 @@ const $wnd = $(window);
 
 // Init infinite scroll pagination.
 $(document).on('initEvents.vpf', (event, self) => {
-	// Exit early if not the right namespace or pagination type
-	if (event.namespace !== 'vpf') {
-		return;
-	}
-
-	// Update pagination options if infinite pagination is present
-	function updatePaginationOptions() {
-		if (
-			self.$pagination.find(
-				'.wp-block-visual-portfolio-pagination-infinite'
-			).length
-		) {
-			self.options.pagination = 'infinite';
-			self.options.nextPageUrl = self.$pagination
-				.find('a.vp-pagination__load-more')
-				.attr('href');
-		}
-	}
-
-	// Initial update of pagination options
-	updatePaginationOptions();
-
-	// Exit if not using infinite pagination
-	if (self.options.pagination !== 'infinite') {
+	if (event.namespace !== 'vpf' || self.options.pagination !== 'infinite') {
 		return;
 	}
 
@@ -37,20 +14,14 @@ $(document).on('initEvents.vpf', (event, self) => {
 	const scrollThreshold = 400;
 	let visibilityCheckBusy = false;
 
-	// Check if we need to load more items
 	function checkVisibilityAndLoad() {
-		// Update pagination options before checking
-		updatePaginationOptions();
-
-		// Skip if busy or no next page URL
 		if (visibilityCheckBusy || !self.options.nextPageUrl) {
 			return;
 		}
-
 		visibilityCheckBusy = true;
+
 		const rect = self.$item[0].getBoundingClientRect();
 
-		// Load more items if we're close to the bottom
 		if (
 			rect.bottom > 0 &&
 			rect.bottom - scrollThreshold <= window.innerHeight
@@ -66,18 +37,15 @@ $(document).on('initEvents.vpf', (event, self) => {
 		}
 	}
 
-	// Create throttled scroll handler using requestAnimationFrame for performance
-	const throttledScrollHandler = throttle(
-		150,
-		rafSchd(checkVisibilityAndLoad)
-	);
-
-	// Initial check
 	checkVisibilityAndLoad();
 
-	// Attach event listeners
 	$wnd.on(
 		`load${evp} scroll${evp} resize${evp} orientationchange${evp}`,
-		throttledScrollHandler
+		throttle(
+			150,
+			rafSchd(() => {
+				checkVisibilityAndLoad();
+			})
+		)
 	);
 });
