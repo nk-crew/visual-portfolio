b7ce4be9e82e7ab19834cb975df743b1c57ecd34
Nikita <nkdevinfo@gmail.com>
2025-10-27 17:26:23 +0300

    build

--- build/assets/js/popup-gallery.asset.php
+++ build/assets/js/popup-gallery.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '0ef264466fb6b2b6e7f2');
+<?php return array('dependencies' => array('jquery'), 'version' => 'd8bdbaf9556b1bef82b7');
