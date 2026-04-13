37a09c218de48ef7f5039abc2b7ad04b947f73b1
Fellan-91 <alexey.fellan@gmail.com>
2026-04-13 16:41:32 +0300

    build: compile button group deprecation cleanup

--- build/gutenberg/index.asset.php
+++ build/gutenberg/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '04ccc087f857164e717e');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '3676bd08edad063e21da');
