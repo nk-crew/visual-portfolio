1aa5b136208856d35449dcb332f0bace166c27b7
Fellan-91 <alexey.fellan@gmail.com>
2026-04-13 16:37:59 +0300

    build: compile legacy saved block alias changes

--- build/gutenberg/index.asset.php
+++ build/gutenberg/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '94a7bf5c117120df9fab');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '04ccc087f857164e717e');
