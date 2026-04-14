2edc5f804d8c8a551afb663bb46cfada93b1a03e
Fellan-91 <alexey.fellan@gmail.com>
2026-04-14 13:22:06 +0300

    build: compile final e2e stability updates

--- build/gutenberg/index.asset.php
+++ build/gutenberg/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f964ac6aaca6dcfc3e64');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '89524de07243d82eb338');
