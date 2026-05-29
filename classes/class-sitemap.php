dd278ebe08fa4843a6b84970ad856aac38506d4c
Fellan-91 <alexey.fellan@gmail.com>
2026-05-29 19:52:42 +0300

    Fix sitemap block attrs fallback

--- classes/class-sitemap.php
+++ classes/class-sitemap.php
@@ -95,8 +95,16 @@ class Visual_Portfolio_Sitemap {
 						'visual-portfolio/saved' === $block['blockName'] ||
 						'nk/visual-portfolio' === $block['blockName']
 					) {
-						$options = Visual_Portfolio_Get::get_options( $block['attrs'] );
-						switch ( $options['content_source'] ) {
+						$block_attrs = isset( $block['attrs'] ) && is_array( $block['attrs'] ) ? $block['attrs'] : array();
+						$options     = Visual_Portfolio_Get::get_options( $block_attrs );
+
+						if ( ! is_array( $options ) ) {
+							$options = array();
+						}
+
+						$content_source = $options['content_source'] ?? ( $block_attrs['content_source'] ?? '' );
+
+						switch ( $content_source ) {
 							case 'post-based':
 								if ( isset( $options['posts_source'] ) ) {
 									$query_opts = Visual_Portfolio_Get::get_query_params( $options, false, $options['id'] );
@@ -125,8 +133,10 @@ class Visual_Portfolio_Sitemap {
 								}
 								break;
 							case 'images':
-								if ( isset( $options['images'] ) ) {
-									foreach ( $options['images'] as $image ) {
+								$images = $options['images'] ?? ( $block_attrs['images'] ?? array() );
+
+								if ( is_array( $images ) ) {
+									foreach ( $images as $image ) {
 										$image_id = $image['id'];
 
 										$image_alt = '';
